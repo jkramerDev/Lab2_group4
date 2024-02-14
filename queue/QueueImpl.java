ddab21af064ad100bf4c6cfdff643ed2c00ddad5
kearamcenroe <159163766+kearamcenroe@users.noreply.github.com>
2024-02-14 16:33:02 -0500

    Update QueueImpl.java

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -1,29 +1,82 @@
 package queue;
-import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueImpl implements Queue {
-	private Queue<String> queue;
-	
-	public QueueImpl() {
-		queue = new LinkedList<>();
-	}
-	public void enQueue(String item) {
-		queue.add(item);
-	}
-	public String deQueue() {
-		if(isEmpty()) {
-			System.out.println("Queue is empty");
+	int LENGTH = 5;
+	String[] arr = new String[LENGTH];
+	int size = 0;
+
+	public boolean isFull() {
+		if (arr[LENGTH - 1] != null) {
+			return true;
 		}
-		return queue.remove();
+		return false;
 	}
+
+	@Override
+	public boolean isEmpty() {
+		if (arr[0] == null) {
+			return true;
+		}
+		return false;
+	}
+
+	public void enQueue(String element) {
+
+		if (isFull()) {
+			System.err.println("sorry, full queue");
+		} else if (isEmpty()) {
+			arr[0] = element;
+			size++;
+		} else {
+			
+			//everybody shift one to the 'right'
+			for (int i = size; i > 0; i--) {
+				arr[i] = arr[i - 1];
+			}
+			
+			//last one on 
+			arr[0] = element;
+			size++;
+		}
+
+	}
+
+	public String deQueue() {
+		String removedPerson = null;
+		if(isEmpty()) {
+			System.out.println("Sorry, empty queue");
+		}else {
+			removedPerson = arr[size -1];
+			for(int i = size -1; i > 0; i--) {
+				arr[i] = arr[i-1];
+			}
+			size--;
+		}
+		return removedPerson;
+	}
+
+	public void display() {
+		if(isEmpty()) {
+			System.out.println("Sorry the queue is empty");
+		}else {
+			System.out.println("People in the queue: ");
+			for(int i = 0; i <size; i++) {
+				System.out.println(arr[i]);
+			}
+		}
+
+	}
+
+	@Override
 	public String peek() {
 		if(isEmpty()) {
-			System.out.println("Queue is empty");
+			System.out.println("Sorry the queue is empty");
+			return null;
+		}else {
+			return arr[size -1];
 		}
-		return queue.peek();
+	
 	}
-	public boolean isEmpty() {
-		return queue.isEmpty();
-	}
+
 }
