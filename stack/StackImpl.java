2029ec3d66cba216382365ebebb035fd3d738abe
kearamcenroe <159163766+kearamcenroe@users.noreply.github.com>
2024-02-15 19:39:55 -0500

    Update StackImpl.java

--- stack/StackImpl.java
+++ stack/StackImpl.java
@@ -1,31 +1,33 @@
 package stack; 
 
 public class StackImpl implements Stack {
-	protected String[] stacks;
-	protected int top;
-	protected int size;
+	private String [] elements = new String[10];
+	private int top = -1;
+	private int size = 10;
 	
-	public StackImpl(int size) {
-		stacks = new String[size];
-		top = -1;
-		this.size = size;
-	} 
-
 	@Override
-	public void push(String s) {
-		if (isFull()) {
-			System.out.println("cannot push any more cuz stack is full");
-			return;
+	public void push(String el) {
+	
+		if (isFull())
+			System.err.println("full stack");
+		else
+		{
+			top++;
+			elements[top] = el;
+	
 		}
-		top++;
-		stacks[top] = s;
 	}
 
 	@Override
 	public String pop() {
-		String person = stacks[top];
-		top--;
-		return person;
+		String poppedElement = null;
+		if(isEmpty()) {
+			System.out.println("Stack is empty");
+		}else {
+			poppedElement = elements[top];
+			top--;
+		}
+		return poppedElement;
 	}
 
 	@Override
@@ -35,33 +37,54 @@ public class StackImpl implements Stack {
 
 	@Override
 	public Boolean isFull() {
+
 		return top == size - 1;
 	}
 
 	@Override
 	public int size() {
-		return size;
+		return top + 1;
 	}
 
 	@Override
 	public String peek() {
-		if (isEmpty()) {
-            return null; 
-        }
-        return stacks[top];
+		if(isEmpty()) {
+			System.out.println("Stack is empty");
+			return null;
+		}else {
+			return elements[top];
+		}
 	}
 
-	@Override 
+	@Override
 	public void setCapacity(int size) {
+		if(size < top + 1) {
+			System.out.println("Size is less than the current number in the stack.");
+		}
+		String [] newSize = new String [size];
+		for(int i = 0; i <= top; i ++) {
+			newSize[i] = elements[i];
+		}
+		elements = newSize;
 		
 	}
 
 	@Override
 	public void display() {
-		System.out.print("[ ");
-		for(int i = 0; i <= top; i++) {
-			System.out.print(stacks[i] + " ");
+		if(isEmpty()) {
+			System.out.println("Stack is empty");
+		}else {
+			System.out.println("Elements in stack: ");
+			for(int i = top; i >= 0; i--) {
+				System.out.println(elements[i]);
+			}
 		}
-		System.out.println("]");
+		
 	}
-}
+ 
+
+
+
+		
+		
+	}
