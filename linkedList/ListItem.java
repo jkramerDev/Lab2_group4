d26961d88ac38a0543edabd4df7ddc595e853687
jooceboks <120229681+jooceboks@users.noreply.github.com>
2024-02-16 22:15:46 -0500

    Update ListItem.java

--- linkedList/ListItem.java
+++ linkedList/ListItem.java
@@ -9,4 +9,19 @@ public class ListItem {
 		this.data = data;
 		this.next = null;
 	}
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public ListItem getNext() {
+		return next;
+	}
+
+	public void setNext(ListItem next) {
+		this.next = next;
+	}
 }
