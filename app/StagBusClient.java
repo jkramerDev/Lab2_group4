78ffeb177128f49ad2d7c67c8ecd07d7e24bcae1
jooceboks <120229681+jooceboks@users.noreply.github.com>
2024-02-21 15:37:43 -0500

    Update StagBusClient.java

--- app/StagBusClient.java
+++ app/StagBusClient.java
@@ -10,21 +10,16 @@ import stack.StackImpl;
 public class StagBusClient {
 
 	public static void main(String[] args) {
-		// create implementation, then
-
 		System.out.println("-----L I S T  T E S T------");
-		LinkedListImpl list = new LinkedListImpl();
+		LinkedListTester.main(args);
 		//listRunTestMethod...
  		
 		System.out.println("-----S T A C K  T E S T------");
-		StackImpl stack = new StackImpl();
+		StackTester.main(args);
 		//QRunTestMethod...
 	 
 		System.out.println("----Q U E U E  T E S T-------");
-		QueueImpl queue = new QueueImpl();
-
+		QueueTester.main(args);
 		//StackRunTestMethod...
-		
-	 	}
 
 }
