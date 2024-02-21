6562fa5a7cc6fa18988ce91f05decad37c60d9f3
kearamcenroe <159163766+kearamcenroe@users.noreply.github.com>
2024-02-21 10:02:01 -0500

    Update StagBusClient.java

--- app/StagBusClient.java
+++ app/StagBusClient.java
@@ -13,15 +13,15 @@ public class StagBusClient {
 		// create implementation, then
 
 		System.out.println("-----L I S T  T E S T------");
-		
+		LinkedListImpl list = new LinkedListImpl();
 		//listRunTestMethod...
  		
 		System.out.println("-----S T A C K  T E S T------");
-
+		StackImpl stack = new StackImpl();
 		//QRunTestMethod...
 	 
 		System.out.println("----Q U E U E  T E S T-------");
-		
+		QueueImpl queue = new QueueImpl();
 
 		//StackRunTestMethod...
 		
