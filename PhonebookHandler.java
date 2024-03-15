008bb2e725a0819117b97c8eeb02e744576b2b6d
jooceboks <120229681+jooceboks@users.noreply.github.com>
2024-03-14 20:59:23 -0400

    Update PhonebookHandler.java

--- PhonebookHandler.java
+++ PhonebookHandler.java
@@ -13,19 +13,42 @@ import java.util.List;
 
 public class PhonebookHandler implements iPhonebookHander{
  
-	public List<Contact> sortByName(List<Contact> contacts){
-		return contacts;
+		Map<Contact, List<PhonebookEntry>> phonebook;
+	
+	public PhonebookHandler(Map<Contact, List<PhonebookEntry>> phonebook) {
+		this.phonebook = phonebook;
+	}
+	
+
+	@Override
+	//bubble sort
+	public List<Contact> sortByName() {
+		List<Contact> sortedContacts = new ArrayList<>();
+		sortedContacts.addAll(phonebook.keySet());
 		
+		for(int current = 0; current < sortedContacts.size() - 1; current++) {
+			for(int index = 0; index < sortedContacts.size() - current - 1; index++) {
+				if((sortedContacts.get(index).getCname()).compareTo(sortedContacts.get(index - 1).getCname()) < 0) {
+					Contact temp = sortedContacts.get(index);
+					sortedContacts.set(index, sortedContacts.get(index - 1));
+					sortedContacts.set((index - 1), temp);
 				}
+			}
+			current++;
+		}
+		return sortedContacts;
+	}
 
 	@Override
 	public List<PhonebookEntry> binarySearch(List<Contact> sortedContacts, String name) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void display(List<Contact> sortedContacts) {
+		for (Contact c : sortedContacts)
+			System.out.print("contact: "+ c.getCname() + ": " + c.getPhonebookEntries());
 		
 	}
-			
-		}
+}
