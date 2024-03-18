e5b11356753117d950d9046671311e4aa6adc215
jooceboks <120229681+jooceboks@users.noreply.github.com>
2024-03-18 00:57:06 -0400

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,6 +1,8 @@
 package template;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 /**
  * model a Contact 
  * Contact has a name & list of phonebook entries
@@ -11,41 +13,52 @@ import java.util.List;
  * 
  */
 
-public class Contact {
-	 private String name;
-	 private List<PhonebookEntry> phonebookEntries;
-	 
-	 public Contact(String name) {
-		 this.name = name;
-		 this.phonebookEntries = new ArrayList<>();
-	 }
-	 public void addPhonebookEntry(PhonebookEntry entry) {
-		 phonebookEntries.add(entry);
-	 }
-	 public String getName() {
-		 return name;
-	 }
-	 public void setName (String name) {
-		 this.name = name;
-	 }
-	 public List<PhonebookEntry> getPhonebookEntries(){
-		 return phonebookEntries;
-	 }
-	 public void setPhonebookEntries(List<PhonebookEntry> phonebookEntries) {
-		 this.phonebookEntries = phonebookEntries;
-	 }
-	@Override
-	public int hashCode() {
-		return name.hashCode();
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if(this == obj)
-			return true;
-		if(obj == null || getClass() != obj.getClass())
-			return false;
-		Contact contact = (Contact)obj;
-		return name.equals(contact.name);
+class Contact {
+	private String Cname; 
+	private ArrayList<PhonebookEntry> Entries;
+	
+	public Contact(String Cname) {
+		this.setCname(Cname);
+		this.Entries = new ArrayList<>();
 	}
 
+	public String getCname() {
+		return Cname;
+	}
+
+	public void setCname(String cname) {
+		this.Cname = cname;
+	}
+	
+	public void addPhonebookEntry(String number, String type) {
+		PhonebookEntry entry = new PhonebookEntry(number, type);
+		Entries.add(entry);
+	}
+	
+
+	public List<PhonebookEntry> getPhonebookEntries() {
+		return Entries;
+	}
+	
+	@Override
+    public String toString() {
+        return "Name: " + Cname  + ", Phone Number(s): " + Entries;
+    }
+	
+	@Override
+	public int hashCode() {
+	    return Objects.hashCode(Cname.length());
+	}
+	
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+        	return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+        	return false;
+        }
+        Contact contact = (Contact)obj;
+        return Cname.equals(contact.Cname);
+    }
 }
